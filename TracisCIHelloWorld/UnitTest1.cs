559dca3153b9ab1c174da4e7ff46b7d292ddc828
Odis Harkins <odis@cucsoft.com>
2017-06-14 12:06:43 -0600

    Try Again

---
+++ TracisCIHelloWorld/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TracisCIHelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4,4);
+        }
+    }
+}
