4a5f9096c82eac79312acc22cb29fba01dc38552
Odis Harkins <odis@cucsoft.com>
2017-06-14 13:49:30 -0600

    Take 5

--- Fibonacci/Fibonacci.Tests/Tests.cs
+++ Fibonacci/Fibonacci.Tests/Tests.cs
@@ -1,32 +1,29 @@
-﻿//using Xunit;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Xunit;
 
 namespace Fibonacci.Tests
 {
-    [TestClass]
     public class Tests
     {
-        [TestMethod]
+        [Fact]
         public void Get_5th_number()
         {
             var generator = new FibonacciGenerator();
 
-            Assert.AreEqual(generator.Fibonacci(5), 5);
+            Assert.Equal(generator.Fibonacci(5), 5);
         }
 
-        [TestMethod]
+        [Fact]
         public void Get_6th_number()
         {
             var generator = new FibonacciGenerator();
 
-            Assert.AreEqual(generator.Fibonacci(6), 8);
+            Assert.Equal(generator.Fibonacci(6), 8);
         }
-        [TestMethod]
+        [Fact]
         public void Get_7th_number()
         {
             var generator = new FibonacciGenerator();
-            Assert.AreEqual(generator.Fibonacci(7), 13);
+            Assert.Equal(generator.Fibonacci(7), 13);
         }
     }
 }
